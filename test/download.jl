0e7f64ecc8171abf05e1af7a56817e7727f7b469
Curtis Vogt <curtis.vogt@gmail.com>
2017-03-20 19:57:58 -0500

    Debugging Travis failure

--- test/download.jl
+++ test/download.jl
@@ -1,16 +1,19 @@
 # This file is a part of Julia. License is MIT: http://julialang.org/license
 
-let file = tempname()
-    @test download("http://httpbin.org/status/200", file) == file
-    @test isfile(file)
-    @test isempty(read(file))
-    rm(file)
-
+let filename = tempname()
     # Make sure that failed downloads do not leave files around
-    @test_throws ErrorException download("http://httpbin.org/status/404", file)
-    @test !isfile(file)
+    @test_throws ErrorException download("http://httpbin.org/status/404", filename)
+    @test !isfile(filename)
+
+    @test Base.downloadcmd == :curl
 
     # Use a TEST-NET (192.0.2.0/24) address which shouldn't be bound
-    @test_throws ErrorException download("http://192.0.2.1", file)
-    @test !isfile(file)
+    @test_throws ErrorException download("http://192.0.2.1", filename)
+    @test !isfile(filename)
+
+    # Download an empty file
+    @test download("http://httpbin.org/status/200", filename) == filename
+    @test isfile(filename)
+    @test isempty(read(filename))
+    rm(filename)
 end
